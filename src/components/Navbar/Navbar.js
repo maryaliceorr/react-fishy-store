04a80f57bcc26cc5fff12be8dfcc326230b23807
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-07 13:25:55 -0500

    added navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 import './Navbar.css';
 
 class Navbar extends React.Component {
   render () {
     return (
       <div className="Navbar">
-        <h2>Navbar</h2>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <Link to="/" className="navbar-brand">Fish Store</Link>
+            </div>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <Link to="/login" className="navbar-brand">Login</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
