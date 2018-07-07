0c244898b7b828ad86232e33a168a2965efbaace
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-07 09:56:16 -0500

    added componentsetup

---
+++ src/components/New/New.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './New.css';
+
+class New extends React.Component {
+  render () {
+    return (
+      <div className="New">
+        <h2>New</h2>
+      </div>
+    );
+  }
+}
+
+export default New;
---
+++ src/components/OrderSpa/OrderSpa.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './OrderSpa.css';
+
+class OrderSpa extends React.Component {
+  render () {
+    return (
+      <div className="OrderSpa">
+        <h2>OrderSpa</h2>
+      </div>
+    );
+  }
+}
+
+export default OrderSpa;
