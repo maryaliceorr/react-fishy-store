b7807dc3f3371414bff9907ef02c2b482a18e552
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-09 18:45:31 -0500

    got files copied

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,41 +1,42 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import authRequests from '../../firebaseRequests/auth';
+
 import './Register.css';
 
 class Register extends React.Component {
   state = {
     user: {
-      email: '',
-      password: '',
+      email: 'zoeames@gmail.com',
+      password: '123456',
     },
   };
 
-registerClickEvent = (e) => {
-  const {user} = this.state;
-  e.preventDefault();
-  authRequests
-  .registerUser(user)
-  .then(() => {
-    this.props.history.push('/orders');
-  })
-  .catch((err) => {
-    console.error('there was an error with Register', err)
-  })
-};
+  registerClickEvent = e => {
+    const { user } = this.state;
+    e.preventDefault();
+    authRequests
+      .registerUser(user)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch(error => {
+        console.error('there was an error in registering', error);
+      });
+  };
 
-emailChange = (e) => {
-  const tempUser = {...this.state.user}
-  tempUser.email = e.target.value;
-  this.setState({user: tempUser});
-};
+  emailChange = e => {
+    const tempUser = { ...this.state.user };
+    tempUser.email = e.target.value;
+    this.setState({ user: tempUser });
+  };
 
-passwordChange = (e) => {
-  const tempUser = {...this.state.user}
-  tempUser.password = e.target.value;
-  this.setState({user: tempUser});
-};
+  passwordChange = e => {
+    const tempUser = { ...this.state.user };
+    tempUser.password = e.target.value;
+    this.setState({ user: tempUser });
+  };
 
   render () {
     const { user } = this.state;
@@ -43,12 +44,12 @@ passwordChange = (e) => {
       <div className="Register">
         <div id="login-form">
           <h1 className="text-center">Register</h1>
-          <form className="form-horizontal col-sm-4 col-sm-offset-4 col-xs-8 col-xs-offset-2">
+          <form className="form-horizontal col-sm-6 col-sm-offset-3">
             <div className="form-group">
-              <label htmlFor="inputEmail" className="col-sm-2 control-label">
-                Email
+              <label htmlFor="inputEmail" className="col-sm-4 control-label">
+                Email:
               </label>
-              <div className="col-sm-10">
+              <div className="col-sm-8">
                 <input
                   type="email"
                   className="form-control"
@@ -60,10 +61,10 @@ passwordChange = (e) => {
               </div>
             </div>
             <div className="form-group">
-              <label htmlFor="inputPassword" className="col-sm-2 control-label">
-                Password
+              <label htmlFor="inputPassword" className="col-sm-4 control-label">
+                Password:
               </label>
-              <div className="col-sm-10">
+              <div className="col-sm-8">
                 <input
                   type="password"
                   className="form-control"
@@ -75,12 +76,12 @@ passwordChange = (e) => {
               </div>
             </div>
             <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10 text-center">
+              <div className="col-sm-12 text-center">
                 <Link to="/login">Need to Login?</Link>
               </div>
             </div>
             <div className="form-group">
-              <div className="col-sm-offset-2 col-sm-10">
+              <div className="col-sm-12">
                 <button
                   type="submit"
                   className="btn btn-default col-xs-12"
@@ -94,7 +95,7 @@ passwordChange = (e) => {
         </div>
       </div>
     );
-  };
-};
+  }
+}
 
-  export default Register;
+export default Register;
