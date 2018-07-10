071a4c4e5fe087b69647a5023fdf27d8422a413f
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-09 19:47:09 -0500

    add fish cards

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import fishRequests from '../../firebaseRequests/fishes';
+import Fish from '../Fish/Fish';
 
 import './Inventory.css';
 
@@ -23,7 +24,10 @@ class Inventory extends React.Component {
   render () {
     const fishComponents = this.state.fishes.map((fish) => {
       return (
-      <h2>{fish.name}</h2>
+      <Fish
+        key={fish.id}
+        details={fish}
+      />
       );
     });
     return (
