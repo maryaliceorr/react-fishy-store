08fe94a7c96f2292637d6822222b01ba88895f35
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-09 19:26:37 -0500

    added fish component

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,12 +1,38 @@
 import React from 'react';
 
+import fishRequests from '../../firebaseRequests/fishes';
+
 import './Inventory.css';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount () {
+    fishRequests
+      .getRequest()
+      .then((fishes) => {
+        this.setState({fishes: fishes});
+        // could also do ({fishes})
+      })
+      .catch((err) => {
+        console.error('error with fish get request', err);
+      })
+  }
   render () {
+    const fishComponents = this.state.fishes.map((fish) => {
+      return (
+      <h2>{fish.name}</h2>
+      );
+    });
     return (
       <div className="Inventory">
         <h2>Inventory</h2>
+        <ul className="fishes">
+          {fishComponents}
+        </ul>
+
       </div>
     );
   }
