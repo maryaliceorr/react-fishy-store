f4a9341c518f08593225d13fd2a0ca4faec9db21
Mary Alice Orr <maryalice@Marys-MacBook-Pro-2.local>
2018-07-10 20:26:21 -0500

    deleted single order

--- src/components/SingleOrder/SingleOrder.js
+++ src/components/SingleOrder/SingleOrder.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import orderRequests from '../../firebaseRequests/orders';
+
 import './SingleOrder.css';
 
 class SingleOrder extends React.Component {
+
+  deleteOrderClick = () => {
+    const firebaseId = this.props.match.params.id;
+    orderRequests
+      .deleteRequest(firebaseId)
+      .then(() => {
+        this.props.history.push('/orders');
+      })
+      .catch((err) => {
+        console.error('error with deleteOrderClick', err);
+      })
+  }
+
   render () {
     return (
       <div className="SingleOrder">
         <h2>SingleOrder</h2>
+        <button className="btn btn-danger" onClick={this.deleteOrderClick}>Delete Order</button>
       </div>
     );
   }
--- src/firebaseRequests/orders.js
+++ src/firebaseRequests/orders.js
@@ -32,7 +32,21 @@ const postRequest = (newOrder) => {
         reject(err);
       })
 
-  })
-}
+  });
+};
 
-export default { getRequest, postRequest };
+const deleteRequest = (orderId) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .delete(`${constants.firebaseConfig.databaseURL}/orders/${orderId}.json`)
+      .then((res) => {
+        resolve(res);
+      })
+      .catch((err) => {
+        reject(err);
+      })
+
+  });
+};
+
+export default { getRequest, postRequest, deleteRequest };
